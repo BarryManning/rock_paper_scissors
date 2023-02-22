97b438a112221586b400aa4aefcbc532e5002e14
Barry <bmannid191@gmail.com>
2023-02-21 20:45:36 -0500

    thought i was finished, but nah

--- script.js
+++ script.js
@@ -5,8 +5,13 @@
 //create a function that randomly chooses the computers choice
 //create a function that compares the users choice to the computers choice
 
+function startGame() {
 
-function playRound() {
+//keep a running score
+    let playerWinCount = 0;
+    let compWinCount = 0;
+
+    function playRound() {
 
     //tool choices
         const possibleChoices = ['rock', 'paper', 'scissors'];
@@ -16,34 +21,55 @@ function playRound() {
             return choices[Math.floor((Math.random()*choices.length))];
         }
 
+    //player choice
+        let playerChoice = convertToLower(prompt('Please choose your tool'));
+        console.log('Player chooses ' + (playerChoice));
+
     //generate computer choice
         let compChoice = (generateRandom(possibleChoices));
         console.log('Computer chooses ' + (compChoice));
 
     //convert to lowercase
-        function convertToLower (choice) {
+        function convertToLower(choice) {
             return choice.toLowerCase();
         }
 
-    //player choice
-        let playerChoice = convertToLower('Paper');
-        console.log('Player chooses ' + (playerChoice));
+    //update winner score
+        function updatePlayerPoint() {
+            playerWinCount++;
+            console.log("Player Score - " + playerWinCount);
+        }
 
-    //compare choices
+        function updateCompPoint() {
+            compWinCount++;
+            console.log("Computer Score - " + compWinCount);
+        }
+
+    //compare choices and update score
         if (playerChoice == 'rock' && compChoice == 'paper') {
             console.log('You have been defeated.');
+            updateCompPoint();
         } else if (playerChoice == 'paper' && compChoice == 'rock') {
             console.log('Congratulations, you win!');
+            updatePlayerPoint();
         } else if (playerChoice == 'paper' && compChoice == 'scissors') {
             console.log('You have been defeated.');
+            updateCompPoint();
         } else if (playerChoice == 'scissors' && compChoice == 'paper') {
             console.log('Congratulations, you win!');
+            updatePlayerPoint();
         } else if (playerChoice == 'scissors' && compChoice == 'rock') {
             console.log('You have been defeated.');
+            updateCompPoint();
         } else {
             console.log("It's a tie!");
         }
     }
 
-    playRound();
+    //create a 5 round loop
+    for (let i = 0; i < 5; i++){
+        playRound();
+    }
+}
 
+startGame();
